f8635da1c402909a2378d108ff6c34c857311ba8
masterchange <2042313869@qq.com>
2024-12-28 13:30:25 +0800

    sandbox demo

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@
 @code target:
 **/
 
-use iced::{Element, Sandbox, Settings};
-use iced::widget::{text};
+use iced::{Element, Length, Sandbox, Settings, Theme};
+use iced::widget::{row, text, text_editor, container, column, horizontal_space};
 use iced::widget::text_editor::Action::Edit;
 
 fn main() -> iced::Result {
@@ -26,12 +26,12 @@ fn main() -> iced::Result {
 }
 
 struct Editor {
-
+    content: text_editor::Content,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Message {
-
+    Edit(text_editor::Action),
 }
 
 impl Sandbox for Editor {
@@ -39,7 +39,7 @@ impl Sandbox for Editor {
 
     fn new() -> Self {
         Self {
-
+            content: text_editor::Content::new(),
         }
     }
 
@@ -48,10 +48,24 @@ impl Sandbox for Editor {
     }
 
     fn update(&mut self, message: Message) {
-        match message { _ => {} }
+        match message {
+            Message::Edit(action) => self.content.perform(action),
+        }
     }
 
     fn view(&self) -> Element<'_, Message> {
-        text("Hello, world!").into()
+        let input_content = text_editor(&self.content).on_action(Message::Edit).height(Length::Fill);
+
+        let position = {
+            let (line, column) = &self.content.cursor_position();
+            text(format!("row:{}, col:{}", line+1, column+1))
+        };
+        let status_bar = row!(horizontal_space(), position);
+
+        container(column!(input_content, status_bar)).padding(10).into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::Dark
     }
 }
